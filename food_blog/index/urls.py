0eda51211af20c867e85b7284e94bec6023543cf
Joshua-li-yi <liyinankai1@163.com>
2020-05-19 20:19:23 +0800

    实现了页面跳转

--- food_blog/index/urls.py
+++ food_blog/index/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from index.views import index
 from index import views
 urlpatterns = [
-    path(r'singup/', views.sigup),
-    path(r'', index)
+    path(r'signup', views.sigup),
+    path(r'', index),
+    path(r'logIn', views.logIn),
+    path(r'home', views.home),
+    path(r'writeblog', views.write_blog),
+    path(r'me', views.me),
 ]
--- food_blog/index/views.py
+++ food_blog/index/views.py
@@ -6,5 +6,21 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+
 def sigup(request):
     return render(request, 'signup.html')
+
+
+def logIn(request):
+    return render(request, 'logIn.html')
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def write_blog(request):
+    return render(request, 'writeblog.html')
+
+def me(request):
+    return render(request, 'me.html')
