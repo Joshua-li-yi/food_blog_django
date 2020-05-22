45c22396646641191a55e6d64c94834655e5a592
Joshua-li-yi <liyinankai1@163.com>
2020-05-22 22:22:45 +0800

    complete logIn page

--- food_blog/user/urls.py
+++ food_blog/user/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from user import views
 urlpatterns = [
     path(r'register', views.register),
-    path(r'login', views.login),
+    path(r'logIn', views.logIn),
     path(r'home', views.home),
 ]
--- food_blog/user/views.py
+++ food_blog/user/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 import pymysql
 
 # Create your views here.
+
+
+# 用户注册
 def register(request):
+
     if request.method == 'POST':
+        # 获取表单中的数据
         email = request.POST.get('userEmail')
         name = request.POST.get('userName')
         pwd = request.POST.get('pwd1')
@@ -11,6 +16,7 @@ def register(request):
         gender = request.POST.get('gender')
 
         print( 'VALUES(1, "{}", 20200920, "{}" , "{}", 20000920, {})'.format(name, email, pwd, gender))
+        # 异常检测
         if name == '':
             message = 'The name can not be none'
             return render(request, 'signup.html', {'message': message})
@@ -20,11 +26,13 @@ def register(request):
         if pwd != pwd2:
             message = 'The password entered twice is different'
             return render(request, 'signup.html', {'message': message})
+
         # 连接数据库
         db = pymysql.connect(host='localhost', port=3306, user='root', passwd='', db='food_blog', charset='utf8')
         print('connect db success')
         # 创建游标
         cursor = db.cursor()
+
         # sql语句
         # （）实现多行字符串连接
         sql = (
@@ -32,6 +40,7 @@ def register(request):
             # {}作为占位符
             'VALUES(2, "{}", 20200920, "{}" , "{}", 20000920, "{}")'.format(name, email, pwd, gender)
         )
+
         try:
             # 执行sql语句
             cursor.execute(sql)
@@ -57,30 +66,33 @@ def register(request):
     elif request.method =='GET':
         return render(request, 'signup.html')
 
-def login(request):
-    if request.method == 'POST':
-        email = request.POST.get('userEmail')
-        name = request.POST.get('userName')
-        pwd = request.POST.get('pwd')
-        # 连接数据库
-        db = pymysql.connect(host='localhost', port=3306, user='root', passwd='', db='food_blog', charset='utf8mb4')
-        # # 创建游标
-        cursor = db.cursor()
-        # # sql语句
-        # sql = """
-        # INSERT INTO user
-        # """
-        # # 执行语句
-        # cursor.execute(sql)
-        # # 关闭游标
-        # cursor.close()
-        # # 关闭数据库连接
-        # db.close()
-        print(email, name, pwd)
-        return render(request, 'home.html')
-    elif request.method =='GET':
-        return render(request, 'signup.html')
 
+# 用户登陆
+def logIn(request):
+    return render(request, 'logIn.html')
+    # if request.method == 'POST':
+    #     # email = request.POST.get('userEmail')
+    #     # name = request.POST.get('userName')
+    #     # pwd = request.POST.get('pwd')
+    #     # 连接数据库
+    #     # db = pymysql.connect(host='localhost', port=3306, user='root', passwd='', db='food_blog', charset='utf8mb4')
+    #     # # 创建游标
+    #     # cursor = db.cursor()
+    #     # # sql语句
+    #     # sql = """
+    #     # INSERT INTO user
+    #     # """
+    #     # # 执行语句
+    #     # cursor.execute(sql)
+    #     # # 关闭游标
+    #     # cursor.close()
+    #     # # 关闭数据库连接
+    #     # db.close()
+    #     # print(email, name, pwd)
+    #     # return render(request, 'home.html')
+    # elif request.method =='GET':
+    #     return render(request, 'logIn.html')
 
+# 用户主页
 def home(request):
     return render(request, 'home.html')
